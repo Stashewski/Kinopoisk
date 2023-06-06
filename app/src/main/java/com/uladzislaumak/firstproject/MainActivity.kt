f8bf1bab272b34a1ed30c3cdd337ee8bf25b58d4
Stashewski <122924829+Stashewski@users.noreply.github.com>
2023-06-06 12:47:56 +0300

    Создан контейнер под меню с кнопками Создан контейнер под нижнее меню Создан контейнер под верхнее меню Сделано задание со звездочкой

--- app/src/main/java/com/uladzislaumak/firstproject/MainActivity.kt
+++ app/src/main/java/com/uladzislaumak/firstproject/MainActivity.kt
@@ -2,12 +2,16 @@ package com.uladzislaumak.firstproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        }
 
+    fun menuButton(view: View) {
+        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
     }
 }
