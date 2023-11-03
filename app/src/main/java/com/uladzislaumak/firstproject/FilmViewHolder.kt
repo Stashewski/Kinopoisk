b341c8b2045d7fdfe08bb2f1631805ebac599fc1
Stashewski <122924829+Stashewski@users.noreply.github.com>
2023-11-03 13:08:32 +0300

    29

---
+++ app/src/main/java/com/uladzislaumak/firstproject/FilmViewHolder.kt
@@ -0,0 +1,31 @@
+package com.uladzislaumak.firstproject
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.film_item.view.*
+
+//В конструктор класс передается layout, который мы создали(film_item.xml)
+class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    //Привязываем view из layout к переменным
+    private val title = itemView.title
+    private val poster = itemView.poster
+    private val description = itemView.description
+
+    //В этом методе кладем данные из film в наши view
+    fun bind(film: Film) {
+        //Устанавливаем заголовок
+        title.text = film.title
+        //Устанавливаем постер
+        //Указываем контейнер, в которм будет "жить" наша картинка
+        Glide.with(itemView)
+            //Загружаем сам ресурс
+            .load(film.poster)
+            //Центруем изображение
+            .centerCrop()
+            //Указываем ImageView, куда будем загружать изображение
+            .into(poster)
+        //Устанавливаем описание
+        description.text = film.description
+    }
+}
