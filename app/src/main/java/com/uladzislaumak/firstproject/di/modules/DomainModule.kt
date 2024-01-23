586916f5563dc27d098628dedb3f695ff0ef9df2
Stashewski <122924829+Stashewski@users.noreply.github.com>
2024-01-23 23:16:31 +0300

    31

--- app/src/main/java/com/uladzislaumak/firstproject/di/modules/DomainModule.kt
+++ app/src/main/java/com/uladzislaumak/firstproject/di/modules/DomainModule.kt
@@ -1,15 +1,29 @@
 package com.amsdevelops.firstproject.di.modules
 
+
+import android.content.Context
+import com.amsdevelops.firstproject.App
 import com.amsdevelops.firstproject.data.MainRepository
 import com.amsdevelops.firstproject.data.TmdbApi
+import com.amsdevelops.firstproject.data.preferenes.PreferenceProvider
 import com.amsdevelops.firstproject.domain.Interactor
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-class DomainModule {
+//Передаем контекст для SharedPreferences через конструктор
+class DomainModule(val context: Context) {
+    //Нам нужно контекст как-то провайдить, поэтому создаем такой метод
+    @Provides
+    fun provideContext() = context
+
     @Singleton
     @Provides
-    fun provideInteractor(repository: MainRepository, tmdbApi: TmdbApi) = Interactor(repo = repository, retrofitService = tmdbApi)
+    //Создаем экземпляр SharedPreferences
+    fun providePreferences(context: Context) = PreferenceProvider(context)
+
+    @Singleton
+    @Provides
+    fun provideInteractor(repository: MainRepository, tmdbApi: TmdbApi, preferenceProvider: PreferenceProvider) = Interactor(repo = repository, retrofitService = tmdbApi, preferences = preferenceProvider)
 }
